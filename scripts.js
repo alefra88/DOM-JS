d84ab7a47bf3452842a435a669c5ae6ae2864a71
alefra88 <efra.rlz@gmail.com>
2023-01-23 01:55:17 -0600

    introducción al DOM y algunas apis

--- scripts.js
+++ scripts.js
@@ -1,1 +1,18 @@
 console.log("hola")
+console.log(window.document)
+console.log(document.head)
+console.log(document.body)
+console.log(document.documentElement)
+console.log(document.doctype)
+console.log(document.characterSet)
+console.log(document.title)
+console.log(document.links)
+console.log(document.images)
+console.log(document.forms)
+console.log(document.styleSheets)
+console.log(document.scripts)
+setTimeout(()=>{
+  console.log(document.getSelection().toString())
+},3000)
+//no es recomendable lo siguiente
+document.write("<h2>Hola mundo desde DOM</h2>")
