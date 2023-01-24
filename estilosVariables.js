9ab9275e54cc975cfa334d380625f14016be2a75
alefra88 <efra.rlz@gmail.com>
2023-01-23 20:53:12 -0600

    aprendiendo a acceder a css desde JS

--- estilosVariables.js
+++ estilosVariables.js
@@ -16,3 +16,23 @@ console.log(getComputedStyle($linkDOM));
 // '.getStyle' es un método antiguo y no estándar de JavaScript que se utilizaba para obtener el estilo de un elemento HTML. No está soportado en todos los navegadores y es mejor evitar su uso.
 
 // '.getComputedStyle' es un método estándar de JavaScript que se utiliza para obtener el estilo calculado (es decir, el estilo final que se aplica a un elemento después de tener en cuenta todas las reglas de estilo y heredar estilos de elementos padres) de un elemento HTML. Este método es compatible con todos los navegadores modernos y es la forma recomendada de obtener el estilo de un elemento.
+
+//variables CSS declaradas en la hoja de estilos
+const $html = document.documentElement,
+    $body = document.body;
+
+let varDarkColor = getComputedStyle($html).getPropertyValue("--dark-color"),
+    varYellowColor = getComputedStyle($html).getPropertyValue("--yellow-color");
+
+//Aplicando las variables al document
+$body.style.backgroundColor = varDarkColor;
+$body.style.color = varYellowColor;
+
+// Modificando variables desde JS
+$html.style.setProperty("--dark-color", "#f1a");
+
+//actualizamos la literal de la variable
+varDarkColor = getComputedStyle($html).getPropertyValue("--dark-color");
+
+//aplicamos la variable modificada al estilo
+$body.style.setProperty("background-color", varDarkColor);
