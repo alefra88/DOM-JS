4389ac1a5fa48f95841843216d51c31abd0a1864
alefra88 <efra.rlz@gmail.com>
2023-01-25 03:23:47 -0600

    primer forma de crear nodos-elementos

--- elements-fragments.js
+++ elements-fragments.js
@@ -1,3 +1,17 @@
-const $figure = document.createElement('figure'),
-  $img = document.createElement('img'),
-  $fig = document.createElement('figcaption');
+const $figure = document.createElement("figure"),
+    $img = document.createElement("img"),
+    $figcaption = document.createElement("figcaption"),
+    $figcaptionText = document.createTextNode("animals"),
+    $cards = document.querySelector(".cards");
+
+$img.setAttribute("src", "https://placeimg.com/200/200/animals");
+$img.setAttribute("alt", "animals");
+$figure.classList.add("card");
+
+$figcaption.appendChild($figcaptionText);
+$figure.appendChild($img);
+$figure.appendChild($figcaption);
+$cards.appendChild($figure);
+
+/////////////////////////////////////////////////////////////////////////
+
