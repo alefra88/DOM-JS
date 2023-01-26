4527ca9b147385f18330d52467bb84db3da3f3a4
alefra88 <efra.rlz@gmail.com>
2023-01-26 00:52:39 -0600

    entendiendo los modificadores de antaño

---
+++ modificando-elements-old-style.js
@@ -0,0 +1,22 @@
+const $cards = document.querySelector(".cards"),
+    $newCard = document.createElement("figure"),
+    $cloneCards = $cards.cloneNode(true);
+
+$newCard.innerHTML = `
+<img src="https://placeimg.com/200/200/any" alt="any">
+<figcaption>Any</figcaption>
+`;
+
+//$newCard.classList.add("card");
+
+// replaceChild sirve para modificar un elemento, es un metodo en deshuso
+//$cards.replaceChild($newCard, $cards.children[2]);
+
+//con el siguiente metodo se inserta antes del nodo
+//$cards.insertBefore($newCard, $cards.firstElementChild);
+
+//con el siguiente metodo podemos eliminar un elemento
+$cards.document.removeChild($cards.lastElementChild);
+
+// el siguiente metodo nos permite clonar nodos
+document.body.appendChild($cloneCards);
