898f83e86cd10861aae10684f3b549f4783c222d
alefra88 <efra.rlz@gmail.com>
2023-01-27 02:50:35 -0600

    entendiendo la propagacion burbuja y la captura

---
+++ flujo-eventos-burbuja-captura.js
@@ -0,0 +1,19 @@
+const $divEventos = document.querySelectorAll(".evento-flujos div");
+
+function flujoEventos(e) {
+    console.log(
+        `Hola ${this.className}, el click lo origino ${e.target.className}`
+    );
+}
+
+$divEventos.forEach((div) => {
+    //la propagacion de burbuja se puede modificar, su valor por defecto es false
+    // div.addEventListener("click", flujoEventos,false);
+    //para invertir la propagacion de burbuja a una propagaciòn de captura se deja en true
+    // div.addEventListener("click", flujoEventos, true);
+    //el tercer parametro tambièn acepta un objeto de entrada
+    div.addEventListener("click", flujoEventos, {
+        capture: true,
+        once: true,
+    });
+});
